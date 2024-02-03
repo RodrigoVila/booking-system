80230be93fa5a200ce7fb5458171b84d17ce9235
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-02-03 20:07:54 +0100

    feat: Add dummy front components

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Button } from "./components/Button";
+
+const client = new QueryClient();
 
 const App = () => {
   return (
-    <div className=" flex h-screen w-full items-center justify-center  bg-green-800 text-white">
-      <Button>asdasd</Button>
-    </div>
+    <QueryClientProvider client={client}>
+      <div className=" flex h-screen w-full items-center justify-center  bg-green-800 text-white">
+        <Button>asdasd</Button>
+      </div>
+    </QueryClientProvider>
   );
 };
 
---
+++ apps/front/src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="flex h-14 w-full items-center justify-center bg-slate-600">
+      Footer
+    </div>
+  );
+};

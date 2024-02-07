384daf082378bcb6229d277a8d7b71168a79c4f0
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-02-07 02:42:44 +0100

    feat(packages): Add TimeslotSchema

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -21,5 +21,30 @@ export const ServiceSchema = z.object({
   ),
 });
 
+export const WorkerSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  specialties: z.array(z.string()).optional(),
+});
+
+export const UserSchema = z.object({
+  email: z.string(),
+  password: z.string().min(1),
+  name: z.string().min(1),
+  lastName: z.string().min(1),
+  bookings: z.array(z.string()),
+});
+
+export const TimeslotSchema = z.object({
+  workerId: z.string(),
+  date: z.date(),
+  startTime: z.string(),
+  endTime: z.string(),
+  isBooked: z.boolean(),
+});
+
 export type BookingType = z.infer<typeof BookingSchema>;
 export type ServiceType = z.infer<typeof ServiceSchema>;
+export type WorkerType = z.infer<typeof WorkerSchema>;
+export type UserType = z.infer<typeof UserSchema>;
+export type TimeslotType = z.infer<typeof TimeslotSchema>;
